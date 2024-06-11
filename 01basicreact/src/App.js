671d16040d9ea35722fb98314897dea16df4920e
SK IMRAN <120629426+IMU-IMRAN@users.noreply.github.com>
2024-06-12 03:02:30 +0530

    basic

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Chai from "./Chai";
+
+//Js ke through html likh pa rhe hai
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur react</h1>
+    </>
+  );
+}
+
+export default App;
